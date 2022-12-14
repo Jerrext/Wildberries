c197fac20fab48ec0750e46da2fbf8010438a039
Alexandra77718 <sandra.davidson777@gmail.com>
2022-12-14 21:16:43 +0300

    start create header

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 //Сюда мы импортируем наши компоненты js
+import { header } from './container/header.js'
+import { header__logo } from './container/header.js'
+import { header__searchInput } from './container/header.js'
+import { header__basket } from './container/header.js'
+
