97e3b1d895c74d0530c3683e62ae8c122d1db72e
Jerrext <113127858+Jerrext@users.noreply.github.com>
2022-12-19 19:44:23 +0300

    [fix] Change card img src

--- src/js/store.js
+++ src/js/store.js
@@ -5,21 +5,21 @@ export { cardData, basketData, sliderData }
 const cardData = [
     {
         id: 1,
-        imgSrc: "/1.dc197a9a.jpg",
+        imgSrc: "https://basket-04.wb.ru/vol438/part43830/43830448/images/big/1.jpg",
         price: 270,
         discount: 90,
         thing: "Гирлянда"
     },
     {
         id: 2,
-        imgSrc: "/2.3ca6bb44.jpg",
+        imgSrc: "https://basket-08.wb.ru/vol1157/part115782/115782018/images/big/1.jpg",
         price: 69,
         discount: 48,
         thing: "Худи"
     },
     {
         id: 3,
-        imgSrc: "/3.a433b89d.jpg",
+        imgSrc: "https://basket-05.wb.ru/vol906/part90623/90623197/images/big/1.jpg",
         price: 253,
         discount: 78,
         thing: "Комбинезон"
