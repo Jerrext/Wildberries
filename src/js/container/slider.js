d60bbaadf3dbbfdb225bfd8e6689ae6a3631b37a
NikaVolkova <nikivolkova041019@gmail.com>
2022-12-19 23:41:17 +0300

    changed slider

--- src/js/container/slider.js
+++ src/js/container/slider.js
@@ -1,15 +1,16 @@
+import {images} from "../store.js"
 const main=document.getElementById('main')
 
 const slayder= document.createElement('div')
 slayder.classList.add("adaptivn-slayder")
 main.appendChild(slayder)
 
-const prev = document.createElement('a')
+const prev = document.createElement('button')
 prev.classList.add('prev')
 prev.innerText="❮"
 slayder.appendChild(prev)
 
-const next = document.createElement('a')
+const next = document.createElement('button')
 next.classList.add('next')
 next.innerText="❯"
 slayder.appendChild(next)
@@ -18,13 +19,6 @@ const kadr= document.createElement("div")
 kadr.classList.add("kadr")
 main.appendChild(kadr)
 
-const images = [
-    'https://images.wbstatic.net/bners1/big_new__22.jpg',
-    'https://images.wbstatic.net/bners1/big_brand_17_12_22.jpg',
-    'https://images.wbstatic.net/bners1/big_cosmetics1.jpg',
-    'https://images.wbstatic.net/bners1/big_toys.jpg',
-    'https://images.wbstatic.net/bners1/big_inditexx_14_11_22.jpg',
-];
 
 images.forEach(src => {
     const img = document.createElement('img');
@@ -54,7 +48,8 @@ function makeTimer(){
    timer = setInterval(function(){
      slideIndex++;
      showSlides(slideIndex);
-   },2000);
+   },3000);
+   
  }
 
  let slideIndex = 1;
@@ -75,6 +70,7 @@ prev.addEventListener('onclick',function(){
 //   makeTimer();
 // })
 
+
   
 function showSlides(n) {
   let i;
@@ -94,5 +90,6 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " deystvuyus";
+  
 }
 
