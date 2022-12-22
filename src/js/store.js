8453789c47da40da5650ebf5158ff0baf229e4cc
Jerrext <113127858+Jerrext@users.noreply.github.com>
2022-12-22 20:17:36 +0300

    1

--- src/js/store.js
+++ src/js/store.js
@@ -23,7 +23,49 @@ const cardData = [
         price: 253,
         discount: 78,
         thing: "Комбинезон"
-    }
+    },
+    {
+        id: 4,
+        imgSrc: "https://basket-04.wb.ru/vol438/part43830/43830448/images/big/1.jpg",
+        price: 270,
+        discount: 90,
+        thing: "Гирлянда"
+    },
+    {
+        id: 5,
+        imgSrc: "https://basket-08.wb.ru/vol1157/part115782/115782018/images/big/1.jpg",
+        price: 69,
+        discount: 48,
+        thing: "Худи"
+    },
+    {
+        id: 6,
+        imgSrc: "https://basket-05.wb.ru/vol906/part90623/90623197/images/big/1.jpg",
+        price: 253,
+        discount: 78,
+        thing: "Комбинезон"
+    },
+    {
+        id: 7,
+        imgSrc: "https://basket-04.wb.ru/vol438/part43830/43830448/images/big/1.jpg",
+        price: 270,
+        discount: 90,
+        thing: "Гирлянда"
+    },
+    {
+        id: 8,
+        imgSrc: "https://basket-08.wb.ru/vol1157/part115782/115782018/images/big/1.jpg",
+        price: 69,
+        discount: 48,
+        thing: "Худи"
+    },
+    {
+        id: 9,
+        imgSrc: "https://basket-05.wb.ru/vol906/part90623/90623197/images/big/1.jpg",
+        price: 253,
+        discount: 78,
+        thing: "Комбинезон"
+    },
 ]
 
 const basketData = []
